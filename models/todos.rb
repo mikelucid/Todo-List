ac705e3fde80daef53446ae8285a99c304575b8d
mikelucid <cleghornm@my.lanecc.edu>
2026-04-19 14:08:57 -0700

    Add Todo model with validations and scopes

--- models/todos.rb
+++ models/todos.rb
@@ -1,1 +1,11 @@
-
+class Todo < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 3, maximum: 100 }
+  validates :description, length: { maximum: 500 }
+  
+  scope :completed, -> { where(completed: true) }
+  scope :incomplete, -> { where(completed: false) }
+  
+  def toggle_completion!
+    update(completed: !completed)
+  end
+end
